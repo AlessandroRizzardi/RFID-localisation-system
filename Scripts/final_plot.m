f423061f67cd4a0f1e2d8a980791be9da0101ff9
Alessandro Rizzardi <alerizza99@gmail.com>
2023-11-08 22:26:45 +0100

    Update final_plot.m

--- Scripts/final_plot.m
+++ Scripts/final_plot.m
@@ -1,23 +1,9 @@
-x_odometry = zeros(length(odometry_history),1);
-y_odometry = zeros(length(odometry_history),1);
-x_dynamics = zeros(length(dynamics_history),1);
-y_dynamics = zeros(length(dynamics_history),1);
-
-for i = 1:length(odometry_history)
-    x_odometry(i) = odometry_history{i,1}(1);
-    y_odometry(i) = odometry_history{i,1}(2);
-
-    x_dynamics(i) = dynamics_history{i,1}(1);
-    y_dynamics(i) = dynamics_history{i,1}(2);
-end
-
-
 angle = 0:0.1:2*pi;
 rad = max_range; %[m]
 x_coord = tag_position(1) + rad*cos(angle);
 y_coord = tag_position(2) + rad*sin(angle);
 
-% Plottings
+% Animations
 for k = 1:steps
     figure(1)
     clf
@@ -32,14 +18,13 @@ for k = 1:steps
 
         plot(robots(i).tag_estimation_history(k,1),tag_estimation_history(k,2),'b*', 'MarkerSize', 10)
     end
-
-
-
-    %legend('Odometry','Dynamics','Tag Position','Best estimation')
+     
     xlabel('x [m]')
     ylabel('y [m]')
+
     xlim([-20,20])
     ylim([-20,20])
+
     title(['Step ', num2str(k)])
     pause(.001)
     
