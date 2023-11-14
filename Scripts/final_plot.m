0f95b6992f1475bf76548aab7402851f8adf0318
Alessandro Rizzardi <alerizza99@gmail.com>
2023-11-15 00:00:26 +0100

    make animation faster

--- Scripts/final_plot.m
+++ Scripts/final_plot.m
@@ -22,17 +22,18 @@ if ANIMATION == true
         for i=1:nRobots
             plot(robots(i).odometry_history{k,1}(1), robots(i).odometry_history{k,1}(2), 'ro')
             plot(robots(i).dynamics_history{k,1}(1), robots(i).dynamics_history{k,1}(2),'bo','MarkerSize', 10, 'LineWidth',2)
-            plot(robots(i).tag_estimation_history{k}(1), robots(i).tag_estimation_history{k}(2), 'c*', 'MarkerSize', 10)
+            plot(robots(i).tag_estimation_history{k}(1), robots(i).tag_estimation_history{k}(2), 'b*', 'MarkerSize', 10)
         end    
     
         xlabel('x [m]')
         ylabel('y [m]')
 
-        xlim([-5, 5])
-        ylim([-5, 5])
+        xlim([-6, 6])
+        ylim([-6, 6])
 
         title(['Step ', num2str(k)])
-        pause(.001)
+        %pause(.001)
+        drawnow
     end
 end
 
@@ -61,11 +62,9 @@ if DRAW == true
         
         x_coord_map = radius_map*cos(angle);
         y_coord_map = radius_map*sin(angle);
-
-
-        % Plottings
         
-        
+        plot(robots(ROBOT).best_tag_estimation(1), robots(ROBOT).best_tag_estimation(2), 'b*','MarkerSize',10)
+
         %plot(x_odometry,y_odometry,'b')
         plot(x_dynamics,y_dynamics, color_drawing(ROBOT))
         plot(tag_position(1),tag_position(2),'r*','MarkerSize',10)
